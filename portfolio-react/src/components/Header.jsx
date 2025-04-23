20224248c63f2181d17661a7de2c014594c59413
vmg22 <victormatiasgarnica@gmail.com>
2025-04-22 23:40:16 -0300

    Actualización de estilos: Header y Footer simplificados

--- portfolio-react/src/components/Header.jsx
+++ portfolio-react/src/components/Header.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
+import React from 'react';
+import "../styles/Header.css";
 
 const Header = () => {
-    let Alumno = " Garnica Matias"
+    let Alumno = " Garnica Matias";
 
+    return (
+        <div className="header">
+            <h1>Portfolio {Alumno}</h1>
+            <hr />
+        </div>
+    );
+};
 
-  return (
-    <div>
-      <h1>Portfolio{Alumno}</h1>
-      <hr />
-
-    </div>
-  )
-}
-
-export default Header
+export default Header;
--- portfolio-react/src/components/Main.jsx
+++ portfolio-react/src/components/Main.jsx
@@ -1,27 +1,41 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import "../styles/Main.css";
 
 const Main = (props) => {
-  return (
-    <div>
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src={props.Alumno.imagen} />
-        <Card.Body>
-          <Card.Title>{props.Alumno.nombre} {props.Alumno.apellido}</Card.Title>
-          <Card.Text>
-            Soy estudiante de la carrera de Programacion Universitaria y hasta el momentos vimos {props.Alumno.lenguajes.map(lenguajes => <span> {lenguajes}</span>)}.
-          </Card.Text>
-
-
-
-          Mi Mascota se llama {props.Alumno.mascotas.map(mascota => <span> {mascota.nombre}</span>)} y es un {props.Alumno.mascotas.map(mascota => <span> {mascota.tipo}</span>)} de {props.Alumno.mascotas.map(mascota => <span> {mascota.edad}</span>)}.
-
-          
-        </Card.Body>
-      </Card>
-    </div>
-  );
+    return (
+        <div className="main-container">
+            <Card className="card">
+                <Card.Img variant="top" src={props.Alumno.imagen} />
+                <Card.Body>
+                    <Card.Title className="card-title">
+                        {props.Alumno.nombre} {props.Alumno.apellido}
+                    </Card.Title>
+                    <Card.Text className="card-text">
+                        Soy estudiante de la carrera de Programación Universitaria y hasta el momento vimos{" "}
+                        {props.Alumno.lenguajes.map((lenguaje, index) => (
+                            <span key={index}> {lenguaje}</span>
+                        ))}
+                        .
+                    </Card.Text>
+                    Mi Mascota se llama{" "}
+                    {props.Alumno.mascotas.map((mascota, index) => (
+                        <span key={index}> {mascota.nombre}</span>
+                    ))}{" "}
+                    y es un{" "}
+                    {props.Alumno.mascotas.map((mascota, index) => (
+                        <span key={index}> {mascota.tipo}</span>
+                    ))}{" "}
+                    de{" "}
+                    {props.Alumno.mascotas.map((mascota, index) => (
+                        <span key={index}> {mascota.edad}</span>
+                    ))}
+                    .
+                </Card.Body>
+            </Card>
+        </div>
+    );
 };
 
 export default Main;
